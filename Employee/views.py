6149f4b4a87bbb317991723cac3a5b0e9865c7a8
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-15 20:10:25 +0530

    fetch employee details

--- Employee/views.py
+++ Employee/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from Employee.models import Employee
 
 # Create your views here.
+def employee_detail(request , pk):
+    employee = get_object_or_404(Employee , pk = pk)
+    return HttpResponse(employee)
