3aae695b73289134810d0edda1948bec73462453
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-15 17:50:26 +0530

    models creation for db

--- Employee/models.py
+++ Employee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length= 50 )
+    last_name = models.CharField(max_length = 50 )
+    image = models.ImageField(upload_to = "images/")
+    designation = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 100 , unique = True)
+    phone_number = models.CharField(max_length = 15 , blank = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
