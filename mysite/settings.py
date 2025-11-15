55f24519d1ab38c3fea1433c1c30808faa0caff0
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-15 18:49:25 +0530

    django media files

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL ='/media/'
+STATIC_ROOT = BASE_DIR / 'media'
