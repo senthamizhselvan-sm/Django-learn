1e56014eab9d7c3c7678a95a4b012f8177b60e6e
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-15 19:13:34 +0530

    fetch data from database

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,15 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'mysite/static',
+    BASE_DIR / 'mysite/static',
 ]
+
+# Media files (User uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL ='/media/'
-STATIC_ROOT = BASE_DIR / 'media'
